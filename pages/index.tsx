1d37170816a308a054e5de7b974122755796969c
longhan2109 <namlong9012@gmail.com>
2022-10-02 10:37:51 +0700

    init next project

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>NFT Marketplace</title>
+      </Head>
+      Hello world
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/sell-nft.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+const SellPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>NFT Marketplace</title>
+      </Head>
+      Sell NFT
+    </div>
+  );
+};
+
+export default SellPage;
